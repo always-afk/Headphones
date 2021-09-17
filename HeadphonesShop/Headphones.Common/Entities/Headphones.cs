dcd8049ee0ad40a70f41c927d7d1fbb3251c8eff
Артём Кармальков <artemkarmalkov@gmail.com>
2021-09-17 08:03:46 +0300

    init commit

---
+++ HeadphonesShop/Headphones.Common/Entities/Headphones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Headphones.Common.Entities
+{
+    public class Headphones
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public Design Design { get; set; }
+        public double MinFrequency { get; set; }
+        public double MaxFrequency { get; set; }
+        public string Picture { get; set; }
+    }
+}
